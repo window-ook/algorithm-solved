68f919b63f2f849036b002f4962ba8d07f8d0df1
windowook <112608822+window-ook@users.noreply.github.com>
2025-01-10 13:14:01 +0900

    [Silver I] Title: 극장 좌석, Time: 92 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Silver/2302. 극장 좌석/극장 좌석.js
+++ 백준/Silver/2302. 극장 좌석/극장 좌석.js
@@ -2,21 +2,17 @@ let file = require('fs').readFileSync('/dev/stdin');
 let input = file.toString().split('\n');
 let n = Number(input[0]);
 let m = Number(input[1]);
-let fix = m > 0 ? input.slice(2, 2 + m).map(Number) : [];
-let vip = [0, ...fix, n + 1]; // 0, 4, 7, 10 섹션을 위해서
+let fix = m > 0 ? input.slice(2, 2 + m).map(Number) : []; // m이 0일 수도 있음
+let vip = [0, ...fix, n + 1];
 let sections = [];
 for (let i = 1; i < vip.length; i++) sections.push(vip[i] - vip[i - 1] - 1);
 
 let d = Array(n + 1).fill(0);
 d[1] = 1;
 d[2] = 2;
-for (let i = 3; i <= n; i++) d[i] = d[i - 2] + d[i - 1];
+for (let i = 3; i <= n; i++) d[i] = d[i - 1] + d[i - 2];
 
 let result = 1;
-for (let i = 0; i < sections.length; i++) {
-  if (sections[i] > 0) {
-    let num = d[sections[i]];
-    result *= num;
-  }
-}
+for (let x of sections) if (x > 0) result *= d[x];
+
 console.log(result);
