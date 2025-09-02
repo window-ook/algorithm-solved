2713b0d58e66b28914f368166adab50801c42389
LEE CHANGWOOK <112608822+window-ook@users.noreply.github.com>
2025-09-02 13:29:56 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.08 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,7 @@
+function solution(a, b, c) {
+    let answer = 0;
+    if (a !== b && a !== c && b !== c) answer = a + b + c;
+    else if (a !== b || a !== c || b !== c ) answer = (a + b + c) * (a**2 + b**2 + c**2);
+    else if (a === b && a === c && b === c ) answer = (a + b + c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3);
+    return answer;
+}
