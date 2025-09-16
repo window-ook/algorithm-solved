db8246a230513fe5bc0b3c01fe6cbc8cbc2a5f8c
LEE CHANGWOOK <cwl64658@gmail.com>
2025-09-16 20:24:20 +0900

    [level 0] Title: 1로 만들기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
@@ -0,0 +1,11 @@
+function solution(num_list) {
+    var answer = 0;
+    for (let i = 0; i < num_list.length; i++) {
+        while (num_list[i] !== 1) {
+            if (num_list[i] % 2 === 0 && num_list[i] > 1) num_list[i] /= 2;
+            else if (num_list[i] % 2 !== 0 && num_list[i] > 1) num_list[i] = (num_list[i] - 1) / 2;
+            answer++;
+        }
+    }
+    return answer;
+}
