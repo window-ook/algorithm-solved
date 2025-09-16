bcb134c8a7b5180f1572946d1d597ffe9bf620a4
LEE CHANGWOOK <cwl64658@gmail.com>
2025-09-16 21:59:22 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 0.23 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.js
@@ -0,0 +1,11 @@
+function solution(arr, flag) {
+    var answer = []; // x
+    for (let i = 0; i < flag.length; i++) {
+        if (flag[i]) {
+            for (let j = 0; j < arr[i] * 2; j++) answer.push(arr[i]);    
+        } else {
+            for (let j = 0; j < arr[i]; j++) answer.pop();
+        }
+    }
+    return answer;
+}
