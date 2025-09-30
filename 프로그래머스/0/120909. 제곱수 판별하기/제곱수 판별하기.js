f71e54fbb2ce745d897085aafd7fe4a9a42bffe5
LEE CHANGWOOK <cwl64658@gmail.com>
2025-09-30 23:32:09 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    var answer = 0;
+    if (Math.sqrt(n) === Math.floor(Math.sqrt(n))) {
+        return answer=1
+    } else {
+        return answer=2
+    }
+    return answer;
+}
