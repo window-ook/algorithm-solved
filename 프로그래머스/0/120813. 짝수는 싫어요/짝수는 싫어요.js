46d5def3fd4f7dfd5f8356eaa129f40afa8a9f8a
LEE CHANGWOOK <cwl64658@gmail.com>
2025-10-31 23:42:02 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    let answer = [];
+    for (let i = 0; i<=n; i++) {
+        if (i % 2 == 1) {
+            answer.push(i)
+        }
+    }
+    answer.sort((a,b) => a - b);
+    return answer;
+}
