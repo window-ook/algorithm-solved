2b7af54e4d210cde05ee27879854b07611636d0c
LEE CHANGWOOK <cwl64658@gmail.com>
2025-11-21 23:33:59 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,16 @@
+function solution(price) {
+    let answer = 0;
+    
+    if (price >= 500000) {
+        answer = price * 0.8
+    } else if (price >= 300000) {
+        answer = price * 0.9
+    } else if (price >= 100000) {
+        answer = price * 0.95
+    } else {
+        return price
+    }
+    
+    answer = Math.trunc(answer)
+    return answer
+}
