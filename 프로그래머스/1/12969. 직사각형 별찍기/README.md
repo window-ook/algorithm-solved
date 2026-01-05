c1c4dcbcebb1a8efd1ccd1588201b1c05fc45ff9
LEE CHANGWOOK <cwl64658@gmail.com>
2026-01-05 09:35:25 +0900

    [level 1] Title: 직사각형 별찍기, Time: 294.44 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/README.md
+++ 프로그래머스/1/12969. 직사각형 별찍기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 직사각형 별찍기 - 12969 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12969) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12969?language=javascript) 
 
 ### 성능 요약
 
-메모리: 32 MB, 시간: 50.79 ms
+메모리: 31.5 MB, 시간: 294.44 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 02일 11:43:44
+2026년 01월 05일 09:35:22
 
 ### 문제 설명
 
--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -2,8 +2,8 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
-    let row = [];
-    for (let i = 0; i < a; i++) row.push('*');
-    let answer = row.join('');
-    for (let i = 0; i < b; i++) console.log(answer);
+    for (let i = 0; i < b; i++) {
+        let row = new Array(a).fill('*').join('');
+        console.log(row);
+    }
 });
