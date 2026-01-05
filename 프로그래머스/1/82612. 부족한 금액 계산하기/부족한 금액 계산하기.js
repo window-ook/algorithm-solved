1d175d32e5629e83ad227735fc21b783fe331e5a
LEE CHANGWOOK <cwl64658@gmail.com>
2026-01-05 10:00:51 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -1,6 +1,5 @@
 function solution(price, money, count) {
-    let sum = [];
-    for (let i = 1; i <= count; i++) sum.push(price * i);
-    sum = sum.reduce((a, b) => a + b, 0);
-    return Math.max(0, sum - money);
+    let result = 0;
+    for (let i = 1; i <= count; i++) result += price * i;
+    return (result - money) > 0 ? (result - money) : 0;
 }
