437cb2c8f5eed21c6b8337b2122add928207d17c
LEE CHANGWOOK <cwl64658@gmail.com>
2026-01-05 11:05:23 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.28 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.js
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.js
@@ -1,31 +1,26 @@
-// n × n 크기의 2차원 배열 초기화
-// 현재 위치 (0, 0)에서 시작
-// 1부터 n² 까지 숫자를 하나씩 채우면서:
-//   - 현재 방향으로 이동 가능하면 계속 진행
-//   - 불가능하면 다음 방향으로 전환
-
 function solution(n) {
-    let answer = Array.from({ length: n }, () => Array(n).fill(0));
-    // 시계 방향으로 꺾기: 우, 하, 좌, 상
-    let dx = [0, 1, 0, -1];
-    let dy = [1, 0, -1, 0];
+    let arr = Array.from({ length: n}, () => new Array(n).fill(0));
     let x = 0;
     let y = 0;
+    let dx = [0,1,0,-1]; // 우하좌상
+    let dy = [1,0,-1,0];
     let dir = 0;
     
-    for (let num = 1; num <= n*n; num++) {
-        answer[x][y] = num;
+    for (let i = 1; i <= n * n; i++) {
+        arr[x][y] = i;
         let nx = x + dx[dir];
         let ny = y + dy[dir];
-        
-        if (nx < 0 || ny < 0 || nx >= n || ny >= n || answer[nx][ny] !== 0) {
+    
+        // 이동 불가(맵 이탈, 이미 방문한 곳)
+        if (nx < 0 || nx >= n || ny < 0 || ny >= n || arr[nx][ny] !== 0) {
             dir = (dir + 1) % 4;
             nx = x + dx[dir];
-            ny = y + dy[dir];                 
+            ny = y + dy[dir];
         }
         
         x = nx;
         y = ny;
     }
-    return answer;
+    
+    return arr;
 }
